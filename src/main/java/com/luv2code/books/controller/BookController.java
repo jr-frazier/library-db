572dc8b09d0297b59e572be09eb97d72b1cad0b3
JR Frazier <justinfrazier@jrs-mbp-2.mynetworksettings.com>
2025-06-23 21:41:38 -0400

    added validation and updated response codes

--- src/main/java/com/luv2code/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -2,6 +2,9 @@ package com.luv2code.books.controller;
 
 
 import com.luv2code.books.entity.Book;
+import com.luv2code.books.request.BookRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,13 +20,13 @@ public class BookController {
 
     private void initializeBooks() {
         books.addAll(List.of(
-                new Book("The Hobbit", "JRR Tolken", "Fantasy"),
-                new Book("The Lord of the Rings", "JRR Tolken", "Fantasy"),
-                new Book("The Hunger Games", "Suzane Collins", "Action"),
-                new Book("To Kill a Mockingbird", "Harper Lee", "Classic"),
-                new Book("1984", "George Orwell", "Science Fiction"),
-                new Book("Pride and Prejudice", "Jane Austen", "Romance"),
-                new Book("The Great Gatsby", "F. Scott Fitzgerald", "Classic")
+                new Book(1, "The Hobbit", "JRR Tolken", "Fantasy", 5),
+                new Book(2, "The Lord of the Rings", "JRR Tolken", "Fantasy", 5),
+                new Book(3, "The Hunger Games", "Suzane Collins", "Action", 4),
+                new Book(4, "To Kill a Mockingbird", "Harper Lee", "Classic", 3),
+                new Book(5, "1984", "George Orwell", "Science Fiction", 4),
+                new Book(6, "Pride and Prejudice", "Jane Austen", "Romance", 3),
+                new Book(7, "The Great Gatsby", "F. Scott Fitzgerald", "Classic", 3)
         ));
     }
 
@@ -31,6 +34,7 @@ public class BookController {
         initializeBooks();
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<Book> getBooks(@RequestParam(required = false) String category) {
         if (category == null) {
@@ -43,10 +47,12 @@ public class BookController {
     }
 
 
-    @GetMapping("/{title}")
-    public Book getBookByTitle(@PathVariable String title) {
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable @Min(value = 1) long id) {
         return books.stream()
-                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .filter(book -> book.getId() == id)
                 .findFirst()
                 .orElse(null);
     }
@@ -66,27 +72,36 @@ public class BookController {
                 .anyMatch(book -> book.getTitle().equalsIgnoreCase(newBook.getTitle()));
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void addBook(@RequestBody Book newBook) {
-        if (doesBookExist(newBook)) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Book already exists");
-        }
-        createBook(newBook);
+    public void addBook(@Valid @RequestBody BookRequest newBook) {
+       long id = books.isEmpty() ? 1 : books.get(books.size() - 1 ).getId() + 1;
+       Book book = convertToBook(id, newBook);
+
+       books.add(book);
     }
 
-    @PutMapping("/{title}")
-    public void updateBook(@PathVariable String title, @RequestBody Book updatedBook) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void updateBook(@PathVariable @Min(value = 1) long id, @Valid @RequestBody BookRequest bookRequest) {
         for (int i = 0; i < books.size(); i++) {
-            if (books.get(i).getTitle().equalsIgnoreCase(title)) {
+            if (books.get(i).getId() == id) {
+                Book updatedBook = convertToBook(id, bookRequest);
                 books.set(i, updatedBook);
                 return;
             }
         }
     }
 
-    @DeleteMapping("/{title}")
-    public void deleteBook(@PathVariable String title) {
-        books.removeIf(book -> book.getTitle().equalsIgnoreCase(title));
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable @Min(value = 1) long id) {
+        books.removeIf(book -> book.getId() == id);
+    }
+
+
+    private Book convertToBook(long id, BookRequest bookRequest) {
+        return new Book(id, bookRequest.getTitle(), bookRequest.getAuthor(), bookRequest.getCategory(), bookRequest.getRating());
     }
 
 }
---
+++ src/main/java/com/luv2code/books/request/BookRequest.java
@@ -0,0 +1,59 @@
+package com.luv2code.books.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
+public class BookRequest {
+    @Size(min = 1, max = 30, message = "Category must be between 1 and 30 characters")
+    private String title;
+
+    @Size(min = 1, max = 40, message = "Category must be between 1 and 40 characters")
+    private String author;
+
+    @Size(min = 1, max = 30, message = "Category must be between 1 and 30 characters")
+    private String category;
+
+    @Min(value = 1, message = "Rating must be between 1 and 5")
+    @Max(value = 5, message = "Rating must be between 1 and 5")
+    private int rating;
+
+    public BookRequest(String title, String author, String category, int rating) {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
